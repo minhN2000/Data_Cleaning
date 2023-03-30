8bea3d8af4e15dee3d35101f9658f358a33a62ba
minhN2000 <nguye3mt@mail.uc.edu>
2023-03-29 20:07:48 -0400

    Update FE: Footer theme

--- senior design/seniordesign/frontend/src/components/Footer.tsx
+++ senior design/seniordesign/frontend/src/components/Footer.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
+import { useTheme } from "@mui/material/styles";
 
 const Footer: React.FC = () => {
+  const theme = useTheme();
   return (
     <Box display="flex" justifyContent="center" alignItems="center" padding="16px">
-      <Typography variant="body1">Senior Design - Authenticate by Minh Nguyen</Typography>
+      <Typography
+        variant="body1"
+        color = {theme.palette.text.secondary}
+      >
+        Senior Design - Authenticate by Minh Nguyen</Typography>
     </Box>
   );
 };
