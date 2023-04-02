38ff51b0012949f98a78aaa702cf872cb9be4f11
minhN2000 <nguye3mt@mail.uc.edu>
2023-04-02 18:03:49 -0400

    Update BE: sign up/in views

--- senior design/seniordesign/runapp/views.py
+++ senior design/seniordesign/runapp/views.py
@@ -1,10 +1,16 @@
-import json
 import openai
-import pandas as pd
+
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.contrib.auth.models import User
+from django.views.generic import TemplateView
 from django.views.decorators.csrf import csrf_exempt
-from validation import validate_csv_file, validate_user_message
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth import get_user_model, authenticate, login
+
+from rest_framework.permissions import AllowAny
+from rest_framework import views, response, permissions, status
+from rest_framework.decorators import api_view, permission_classes
 
 openai.api_key = 'YOU API KEY HERE'
 your_model = 'SORRY I CANNOT SHARE MY MODEL YET'
@@ -50,3 +56,50 @@ def get_response(request):
         return JsonResponse({"answer": answer})
     else:
         return JsonResponse({"error": "Invalid request method"})
+    
+# Sign up view
+class UserRegistrationView(views.APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        if not email or not password:
+            return response.Response({'error': 'Email and password are required'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if User.objects.filter(username=email).exists():
+            return response.Response({'error': 'A user with this email already exists.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        hashed_password = make_password(password)
+        user = User.objects.create(username=email, email=email, password=hashed_password)
+        user.is_active = False
+        user.save()
+
+        return response.Response(status=status.HTTP_201_CREATED)
+    
+    def get(self, request, *args, **kwargs):
+        return TemplateView.as_view(template_name='index.html')(request, *args, **kwargs)
+
+
+# Sign in view
+@api_view(['GET', 'POST'])
+@permission_classes([AllowAny])
+def user_login_view(request):
+    if request.method == 'GET':
+        # Serve the React app's main HTML file
+        return render(request, 'index.html')
+    else:
+        email = request.data.get('email')
+        password = request.data.get('password')
+        user_instance = User.objects.get(username=email)
+    
+        if not email or not password:
+            return response.Response({'error': 'Email and password are required'}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = authenticate(request, username=email, password=password)
+        if user_instance is not None:
+            login(request, user_instance)
+            return response.Response({'status': 'success', 'message': 'User logged in successfully', 'token':email}, status=status.HTTP_200_OK)
+        else:
+            return response.Response({'status': 'error', 'message': 'Invalid email or password'}, status=status.HTTP_400_BAD_REQUEST)
