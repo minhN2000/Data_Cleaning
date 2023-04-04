2ab5c6ecc25fec6eef09a74ec9ab8ad486a056af
minhN2000 <nguye3mt@mail.uc.edu>
2023-04-03 21:21:52 -0400

    update BE: create multiple users permission

--- senior design/seniordesign/runapp/models.py
+++ senior design/seniordesign/runapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser, Group, Permission
+
+class User(AbstractUser):
+    groups = models.ManyToManyField(Group, related_name="runapp_user_set", blank=True)
+    user_permissions = models.ManyToManyField(Permission, related_name="runapp_user_set", blank=True)
